3caeb041a5905a3c0375300df8f8983791f54041
UltimateLahav <lahav201212@gmail.com>
2026-06-20 13:06:08 +0300

    Update start-online.bat

--- start-online.bat
+++ start-online.bat
@@ -14,6 +14,13 @@ echo.
 echo Google Sign-In setup:
 set "GOOGLE_CLIENT_ID=985537852640-mj77hcjhvvpj0at4bmthoek3afbk88og.apps.googleusercontent.com"
 echo Google Sign-In Client ID loaded for this server session.
+if defined APPDATA (
+  set "VOID_LIMIT_DATA_DIR=%APPDATA%\VoidLimit\account-data"
+) else (
+  set "VOID_LIMIT_DATA_DIR=%USERPROFILE%\Documents\VoidLimit\account-data"
+)
+if not exist "%VOID_LIMIT_DATA_DIR%" mkdir "%VOID_LIMIT_DATA_DIR%"
+echo Account save folder: %VOID_LIMIT_DATA_DIR%
 start "" "http://localhost:4173"
 "%NODE%" "%~dp0server.js"
 pause
